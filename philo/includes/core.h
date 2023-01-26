035c0ca550e81339e3e0622c3b2d2a52963c80f8
Nikita VASILEV <vassilev.nikita@gmail.com>
2023-01-26 03:43:34 +0100

    reviewed strucs + init_philos + init_data

--- philo/includes/core.h
+++ philo/includes/core.h
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 21:16:15 by nvasilev          #+#    #+#             */
-/*   Updated: 2023/01/25 23:43:04 by nvasilev         ###   ########.fr       */
+/*   Updated: 2023/01/26 03:35:10 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@
 # include <sys/time.h>
 # include <stdbool.h>
 
-typedef struct s_data	t_data;
 typedef struct s_philo
 {
 	pthread_t		thread_id;
 	unsigned short	id;
 	unsigned int	times_ate;
-	unsigned int	fork[2];
-	unsigned int 	nb_of_meals;
+	unsigned int	nb_of_meals;
+	unsigned short	left_fork;
+	unsigned short	right_fork;
 	time_t			last_meal;
-	t_data			*data;
+	struct s_data	*data;
 }	t_philo;
 
 typedef enum e_state
@@ -51,9 +51,11 @@ typedef struct s_data
 	pthread_mutex_t	check_death_lock;
 	pthread_mutex_t	print_lock;
 	t_state			state;
-	t_philo			**philos;
+	t_philo			*philos;
 }	t_data;
 
 void	*routine(void *philo);
+int		init_philos(t_data *data);
+int		init_data(t_data *data);
 
 #endif
