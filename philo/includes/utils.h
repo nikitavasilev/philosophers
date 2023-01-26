fe83727f5a5e9178cd795c004f3973c9478e0236
Nikita VASILEV <vassilev.nikita@gmail.com>
2023-01-26 03:42:51 +0100

    get_time_ms prot++

--- philo/includes/utils.h
+++ philo/includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 20:09:25 by nvasilev          #+#    #+#             */
-/*   Updated: 2023/01/25 23:44:19 by nvasilev         ###   ########.fr       */
+/*   Updated: 2023/01/26 02:15:22 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ int		atoi_err_overflow(const char *str, int *err);
 int		ft_isdigit(int c);
 int		ft_isspace(int c);
 void	print_args(t_data *data);
+time_t	get_time_ms(void);
 
 #endif
