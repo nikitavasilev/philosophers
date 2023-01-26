14de5288c619dfc0dbd6624830edea9cdebebace
Nikita VASILEV <vassilev.nikita@gmail.com>
2023-01-26 03:39:10 +0100

    norm + new structs

--- philo/core/create_threads.c
+++ philo/core/create_threads.c
@@ -1,18 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_threads.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 03:35:29 by nvasilev          #+#    #+#             */
+/*   Updated: 2023/01/26 03:35:31 by nvasilev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <pthread.h>
 #include <stdlib.h>
 #include "core.h"
 
 // function to create threads
-void	create_threads(t_philo *philos, t_data *data)
+void	create_threads(t_data *data)
 {
 	int			i;
 	pthread_t	*threads;
 
 	threads = malloc(sizeof(pthread_t) * data->nb_philos);
+	if (!threads)
+		return ;
 	i = 0;
 	while (i < data->nb_philos)
 	{
-		pthread_create(&threads[i], NULL, &routine, &philos[i]);
+		pthread_create(&threads[i], NULL, &routine, &data->philos[i + 1]);
 		i++;
 	}
 	i = 0;
