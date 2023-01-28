66f4d29d124fedd367aad23cbd833a892de3ad34
Nikita VASILEV <vassilev.nikita@gmail.com>
2023-01-28 07:06:12 +0100

    ch logic due to struct change

--- philo/core/init.c
+++ philo/core/init.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 01:52:06 by nvasilev          #+#    #+#             */
-/*   Updated: 2023/01/26 02:16:18 by nvasilev         ###   ########.fr       */
+/*   Updated: 2023/01/28 04:23:41 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,9 @@ int	init_philos(t_data *data)
 		philos[i].data = data;
 		philos[i].last_meal = 0;
 		philos[i].nb_of_meals = 0;
-		philos[i].left_fork = i;
-		philos[i].right_fork = (i + 1) % data->nb_philos;
+		philos[i].left_fork_lock = &data->forks_lock[i];
+		philos[i].right_fork_lock = &data->forks_lock[(i + 1) % data->nb_philos];
+		philos[i].state = THINKING;
 		i++;
 	}
 	data->philos = philos;
@@ -42,22 +43,17 @@ int	init_data(t_data *data)
 	unsigned short	i;
 
 	data->start_time = get_time_ms();
-	data->state = THINKING;
 	if (!init_philos(data))
 		return (0);
-	data->left_fork_lock = malloc(sizeof(pthread_mutex_t) * data->nb_philos);
-	if (!data->left_fork_lock)
-		return (0);
-	data->left_right_lock = malloc(sizeof(pthread_mutex_t) * data->nb_philos);
-	if (!data->left_right_lock)
+	data->forks_lock = malloc(sizeof(pthread_mutex_t) * data->nb_philos);
+	if (!data->forks_lock)
 		return (0);
 	pthread_mutex_init(&data->check_death_lock, NULL);
 	pthread_mutex_init(&data->print_lock, NULL);
 	i = 0;
 	while (i < data->nb_philos)
 	{
-		pthread_mutex_init(&data->left_fork_lock[i], NULL);
-		pthread_mutex_init(&data->left_right_lock[i], NULL);
+		pthread_mutex_init(&data->forks_lock[i], NULL);
 		i++;
 	}
 	return (1);
