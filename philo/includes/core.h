f5fdb9c6ec1b441cbe3b0b42e8fd6e3466ad404c
Nikita VASILEV <vassilev.nikita@gmail.com>
2023-01-28 07:07:43 +0100

    forks arr + threaded state

--- philo/includes/core.h
+++ philo/includes/core.h
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 21:16:15 by nvasilev          #+#    #+#             */
-/*   Updated: 2023/01/26 03:35:10 by nvasilev         ###   ########.fr       */
+/*   Updated: 2023/01/28 04:19:53 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,6 @@
 # include <sys/time.h>
 # include <stdbool.h>
 
-typedef struct s_philo
-{
-	pthread_t		thread_id;
-	unsigned short	id;
-	unsigned int	times_ate;
-	unsigned int	nb_of_meals;
-	unsigned short	left_fork;
-	unsigned short	right_fork;
-	time_t			last_meal;
-	struct s_data	*data;
-}	t_philo;
-
 typedef enum e_state
 {
 	TAKING_FORK,
@@ -37,6 +25,18 @@ typedef enum e_state
 	THINKING,
 	DIED
 }	t_state;
+typedef struct s_philo
+{
+	pthread_t		thread_id;
+	unsigned short	id;
+	unsigned int	times_ate;
+	unsigned int	nb_of_meals;
+	pthread_mutex_t	*left_fork_lock;
+	pthread_mutex_t	*right_fork_lock;
+	time_t			last_meal;
+	t_state			state;
+	struct s_data	*data;
+}	t_philo;
 
 typedef struct s_data
 {
@@ -46,16 +46,16 @@ typedef struct s_data
 	time_t			time_to_sleep;
 	unsigned int	nb_tmust_eat;
 	time_t			start_time;
-	pthread_mutex_t	*left_fork_lock;
-	pthread_mutex_t	*left_right_lock;
+	pthread_mutex_t	*forks_lock;
 	pthread_mutex_t	check_death_lock;
 	pthread_mutex_t	print_lock;
-	t_state			state;
 	t_philo			*philos;
 }	t_data;
 
 void	*routine(void *philo);
 int		init_philos(t_data *data);
 int		init_data(t_data *data);
+void	destroy(t_data *data);
+void	create_threads(t_data *data);
 
 #endif
