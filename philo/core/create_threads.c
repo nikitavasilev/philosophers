a20c0ed88b812652e76d13f73ed93c709bae7196
Nikita VASILEV <vassilev.nikita@gmail.com>
2023-01-29 08:28:07 +0100

    odd even dispatch create_threads

--- philo/core/create_threads.c
+++ philo/core/create_threads.c
@@ -6,11 +6,12 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 03:35:29 by nvasilev          #+#    #+#             */
-/*   Updated: 2023/01/28 07:37:07 by nvasilev         ###   ########.fr       */
+/*   Updated: 2023/01/29 05:07:55 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <unistd.h>
 #include "core.h"
 
 void	create_threads(t_data *data)
@@ -25,8 +26,16 @@ void	create_threads(t_data *data)
 	while (i < data->nb_philos)
 	{
 		pthread_create(&threads[i], NULL, &routine, &data->philos[i]);
-		i++;
+		i += 2;
 	}
+	usleep(100);
+	i = 1;
+	while (i < data->nb_philos)
+	{
+		pthread_create(&threads[i], NULL, &routine, &data->philos[i]);
+		i += 2;
+	}
+	check_death(data);
 	i = 0;
 	while (i < data->nb_philos)
 	{
