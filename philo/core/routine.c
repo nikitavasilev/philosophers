25516e3c4629199ebe84c2e4dcade71b0cbcadda
Nikita VASILEV <vassilev.nikita@gmail.com>
2023-01-29 08:26:39 +0100

    actions + mutexes check death

--- philo/core/routine.c
+++ philo/core/routine.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 03:35:42 by nvasilev          #+#    #+#             */
-/*   Updated: 2023/01/28 07:52:47 by nvasilev         ###   ########.fr       */
+/*   Updated: 2023/01/29 05:43:02 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@
 #include "logs.h"
 #include "utils.h"
 
-void	take_forks(t_philo *philo);
+int		take_forks(t_philo *philo);
 void	put_forks(t_philo *philo);
-void	eat(t_philo *philo);
+int		eat(t_philo *philo);
+void	philo_sleep(t_data *data, time_t sleep_time);
 
 void	*routine(void *philo)
 {
@@ -28,27 +29,65 @@ void	*routine(void *philo)
 	philo_ptr = (t_philo *)philo;
 	while (1)
 	{
-		take_forks(philo_ptr);
-		eat(philo_ptr);
+		pthread_mutex_lock(&philo_ptr->data->check_death_lock);
+		if (philo_ptr->data->one_died)
+		{
+			pthread_mutex_unlock(&philo_ptr->data->check_death_lock);
+			return (NULL);
+		}
+		pthread_mutex_unlock(&philo_ptr->data->check_death_lock);
+		if (!take_forks(philo_ptr))
+			return (NULL);
+		if (!eat(philo_ptr))
+			return (NULL);
 		put_forks(philo_ptr);
 		philo_ptr->state = SLEEPING;
+		pthread_mutex_lock(&philo_ptr->data->check_death_lock);
+		if (philo_ptr->data->one_died)
+		{
+			pthread_mutex_unlock(&philo_ptr->data->check_death_lock);
+			return (NULL);
+		}
+		pthread_mutex_unlock(&philo_ptr->data->check_death_lock);
 		print_message(philo_ptr);
-		usleep(philo_ptr->data->time_to_sleep * 1000);
+		philo_sleep(philo_ptr->data, philo_ptr->data->time_to_sleep);
+		pthread_mutex_lock(&philo_ptr->data->check_death_lock);
+		if (philo_ptr->data->one_died)
+		{
+			pthread_mutex_unlock(&philo_ptr->data->check_death_lock);
+			return (NULL);
+		}
+		pthread_mutex_unlock(&philo_ptr->data->check_death_lock);
 		philo_ptr->state = THINKING;
 		print_message(philo_ptr);
 	}
 }
 
-void	take_forks(t_philo *philo)
+int	take_forks(t_philo *philo)
 {
 	pthread_mutex_t	*forks_lock;
 
 	forks_lock = philo->data->forks_lock;
 	pthread_mutex_lock(&forks_lock[philo->id - 1]);
 	philo->state = TAKING_FORK;
+	pthread_mutex_lock(&philo->data->check_death_lock);
+	if (philo->data->one_died)
+	{
+		pthread_mutex_unlock(&philo->data->check_death_lock);
+		return (0);
+	}
+	pthread_mutex_unlock(&philo->data->check_death_lock);
 	print_message(philo);
 	pthread_mutex_lock(&forks_lock[philo->id % philo->data->nb_philos]);
+	pthread_mutex_lock(&philo->data->check_death_lock);
+	if (philo->data->one_died)
+	{
+		pthread_mutex_unlock(&philo->data->check_death_lock);
+		return (0);
+	}
+	pthread_mutex_unlock(&philo->data->check_death_lock);
 	print_message(philo);
+	return (1);
 }
 
 void	put_forks(t_philo *philo)
@@ -60,11 +99,37 @@ void	put_forks(t_philo *philo)
 	pthread_mutex_unlock(&forks_lock[philo->id % philo->data->nb_philos]);
 }
 
-void	eat(t_philo *philo)
+int	eat(t_philo *philo)
 {
 	philo->state = EATING;
 	philo->last_meal = get_time_ms();
 	philo->nb_of_meals++;
+	pthread_mutex_lock(&philo->data->check_death_lock);
+	if (philo->data->one_died)
+	{
+		pthread_mutex_unlock(&philo->data->check_death_lock);
+		return (0);
+	}
+	pthread_mutex_unlock(&philo->data->check_death_lock);
 	print_message(philo);
-	usleep(philo->data->time_to_eat * 1000);
+	philo_sleep(philo->data, philo->data->time_to_eat);
+	return (1);
+}
+
+void	philo_sleep(t_data *data, time_t sleep_time)
+{
+	time_t	wake_up;
+
+	wake_up = get_time_ms() + sleep_time;
+	while (get_time_ms() < wake_up)
+	{
+		pthread_mutex_lock(&data->check_death_lock);
+		if (data->one_died)
+		{
+			pthread_mutex_unlock(&data->check_death_lock);
+			break ;
+		}
+		pthread_mutex_unlock(&data->check_death_lock);
+		usleep(100);
+	}
 }
