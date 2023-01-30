49ac3bf2f9c5777de505bbf97d4632b2f53359a8
Nikita VASILEV <vassilev.nikita@gmail.com>
2023-01-30 15:40:06 +0100

    print_death + fix dataraces

--- philo/display/logs.c
+++ philo/display/logs.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 21:04:04 by nvasilev          #+#    #+#             */
-/*   Updated: 2023/01/30 00:42:51 by nvasilev         ###   ########.fr       */
+/*   Updated: 2023/01/30 14:57:27 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ int	print_message(t_philo *philo)
 
 	current_time = get_time_ms();
 	start_time = philo->data->start_time;
-	pthread_mutex_lock(&philo->data->print_lock);
-	pthread_mutex_lock(&philo->data->state_lock);
 	pthread_mutex_lock(&philo->data->check_death_lock);
 	if (!philo->data->one_died)
 	{
+		pthread_mutex_lock(&philo->data->print_lock);
+		pthread_mutex_lock(&philo->data->state_lock);
 		if (philo->state == TAKING_FORK)
 			printf("%zu %d %s\n", current_time - start_time, philo->id, LOG_FORK);
 		else if (philo->state == EATING)
@@ -35,17 +35,26 @@ int	print_message(t_philo *philo)
 			printf("%zu %d %s\n", current_time - start_time, philo->id, LOG_SLEEP);
 		else if (philo->state == THINKING)
 			printf("%zu %d %s\n", current_time - start_time, philo->id, LOG_THINK);
-	}
-	if (philo->state == DIED)
-	{
-		printf("%zu %d %s\n", current_time - start_time, philo->id, LOG_DIE);
-		pthread_mutex_unlock(&philo->data->check_death_lock);
 		pthread_mutex_unlock(&philo->data->state_lock);
 		pthread_mutex_unlock(&philo->data->print_lock);
+	}
+	else
+	{
+		pthread_mutex_unlock(&philo->data->check_death_lock);
 		return (0);
 	}
 	pthread_mutex_unlock(&philo->data->check_death_lock);
-	pthread_mutex_unlock(&philo->data->state_lock);
-	pthread_mutex_unlock(&philo->data->print_lock);
 	return (1);
+}
+
+void	print_death(t_philo *philo)
+{
+	time_t	current_time;
+	time_t	start_time;
+
+	current_time = get_time_ms();
+	start_time = philo->data->start_time;
+	pthread_mutex_lock(&philo->data->print_lock);
+	printf("%zu %d %s\n", current_time - start_time, philo->id, LOG_DIE);
+	pthread_mutex_unlock(&philo->data->print_lock);
 }
